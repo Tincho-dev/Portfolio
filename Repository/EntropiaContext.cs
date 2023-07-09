9f64bbd424178a485c23246195c83fc14e75ed19
Martin Lopez Rubio <Martin.LopezRubio@alu.frt.utn.edu.ar>
2023-07-09 13:20:51 -0300

    histograma comentado por errorer en la renderizacion

--- Repository/EntropiaContext.cs
+++ Repository/EntropiaContext.cs
@@ -1,23 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Models;
-using System.Collections.Generic;
-using System.Reflection.Emit;
 
-namespace EntropiaBlazor.Data
+namespace EntropiaBlazor.Data;
+
+public class EntropiaContext : DbContext
 {
-    public class EntropiaContext : DbContext
+    public EntropiaContext(DbContextOptions<EntropiaContext> options) : base(options)
     {
-        public EntropiaContext(DbContextOptions<EntropiaContext> options) : base(options)
-        {
 
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Fuente>().HasData(
-                new Fuente { CadenaFuente = "Esta es una fuente ya en memoria" },
-                new Fuente { CadenaFuente = "Este texto representa el libro del quijote de la mancha" }
-            );
-        }
-        public DbSet<Fuente> Fuentes => Set<Fuente>();
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Fuente>().HasData(
+            new Fuente { CadenaFuente = "Esta es una fuente ya en memoria" },
+            new Fuente { CadenaFuente = "Este texto representa el libro del quijote de la mancha" }
+        );
+    }
+    public DbSet<Fuente> Fuentes => Set<Fuente>();
 }
+
